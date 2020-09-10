be8ba16292e94ed696bf72b6aaf08554eed4d4b9
tyler belmonte <tylerbelmonte@gmail.com>
2020-09-09 20:36:24 -0500

    second

---
+++ src/Controllers/Redux/BugSlice.js
@@ -0,0 +1,21 @@
+import {createSlice} from "@reduxjs/toolkit"
+
+const slice = createSlice({
+    name: "bug",
+    initialState:[],
+    reducers:{
+        getBugs:(state) => {
+
+        },
+        createBugs:(state,actions) => {
+
+        },
+        updateBug:(state,actions) => {
+
+        },
+        markComplete:(state,actions) => {
+
+        }
+        
+    }
+})
